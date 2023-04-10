b66997c652f5a34dc9c1377e0c2372090a8e5e34
Sandystar <535789035@qq.com>
2023-04-10 22:45:13 +0800

    il2cpp string

--- module/src/main/cpp/il2cpp_lua.cpp
+++ module/src/main/cpp/il2cpp_lua.cpp
@@ -2,7 +2,7 @@
 // Created by Perfare on 2020/7/4.
 //
 
-#include "utf8.h"
+#include "il2cpp_string.h"
 #include "il2cpp_lua.h"
 #include <jni.h>
 #include "hack.h"
@@ -60,30 +60,6 @@ void il2cpp_api_init(void *handle) {
     }
     auto domain = il2cpp_domain_get();
     il2cpp_thread_attach(domain);
-}
-
-std::string Utf16ToUtf8(const Il2CppChar *utf16String, int maximumSize) {
-    const Il2CppChar *ptr = utf16String;
-    size_t length = 0;
-    while (*ptr) {
-        ptr++;
-        length++;
-        if (maximumSize != -1 && length == maximumSize)
-            break;
-    }
- 
-    std::string utf8String;
-    utf8String.reserve(length);
-    utf8::unchecked::utf16to8(utf16String, ptr, std::back_inserter(utf8String));
- 
-    return utf8String;
-}
-std::string Utf16ToUtf8(const Il2CppChar *utf16String) {
-    return Utf16ToUtf8(utf16String, -1);
-}
-std::string Il2CppStringToStdString(Il2CppString *str) {
-    auto chars = il2cpp_string_chars(str);
-    return Utf16ToUtf8(chars);
 }
 
 void hack_lua() {
